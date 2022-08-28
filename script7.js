14020011f06b33b5221e45dbb7b72e524c87ca8f
Rafiya <92360449+Rafiyathanvir@users.noreply.github.com>
2022-08-28 22:27:02 +0530

    Update script7.js

--- script7.js
+++ script7.js
@@ -4,10 +4,10 @@ b.Get all the countries with a population of less than 2 lakhs using Filter func
 c.Print the following details name, capital, flag using forEach function
 d.Print the total population of countries using reduce function
 e.Print the country which uses US Dollars as currency.*/
-
+///////////////////////////////////////////////////////////////////////////////////////////////////
 //a)Get all the countries from the Asia continent /region using the Filter function?
 
-/*var request=new XMLHttpRequest();
+var request=new XMLHttpRequest();
 
 request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
 request.send();
@@ -19,11 +19,11 @@ request.onload=function(){
     {
         console.log(country)
     }
-       }*/
-     ///////////////////////////////////////////
+       }
+     ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
   //   b.Get all the countries with a population of less than 2 lakhs using Filter function?
-  /*var request=new XMLHttpRequest();
+  var request=new XMLHttpRequest();
 
 request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
 request.send();
@@ -36,12 +36,12 @@ request.onload=function(){
         console.log(country)
     }
        
-     }*/
+     }
 
-/////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
 
 //c. Print the following details name, capital, flag using forEach function?
-    /* var request=new XMLHttpRequest();
+     var request=new XMLHttpRequest();
 
      request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
      request.send();
@@ -56,13 +56,14 @@ request.onload=function(){
          }
        
             
-         )};*/
+         )};
+///////////////////////////////////////////////////////////////////////////////////////
 
  // d.Print the total population of countries using reduce function?
 
         
     
- /*var request=new XMLHttpRequest();
+ var request=new XMLHttpRequest();
 
  request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
  request.send();
@@ -80,10 +81,12 @@ request.onload=function(){
         
        },0)
        console.log(poptot)
-    }*/
+    }
+
+///////////////////////////////////////////////////////////////////////////////////////
 
     //e.Print the country which uses US Dollars as currency.*/
-    var request=new XMLHttpRequest();
+  var request=new XMLHttpRequest();
 
     request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
     request.send();
@@ -91,11 +94,23 @@ request.onload=function(){
         var result=JSON.parse(request.response)
         console.log(result)
     
-        var country=result.filter((fun)=>fun.currencies==="$" )
+        for(var i=0;i<result.length;i++)
         {
-            console.log(country)
+        
+            if((result[i].currencies.length>0))
+            {
+                let currarr=result[i].currencies
+                for(let j=0;j<currarr.length;j++){
+                    if(currarr[j].code==="USD"){
+                        console.log(result[i].name)
+                    }
+                }
+               
+            }
         }
            
-         }
+           
+         
+        }
 
          //code: 'USD', name: 'United State Dollar', symbol: '$'
